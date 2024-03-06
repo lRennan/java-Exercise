d147dbb36fe69fdb8da410d9f41c3b684392e23f
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:02:41 -0300

    Update exercise2.java

--- exercise2.java
+++ exercise2.java
@@ -6,22 +6,16 @@ import java.util.Scanner;
 
 public class exercise2 {
     public static void main(String[] args) {
-        // Criar um Scanner para ler o valor do teclado
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar ao usuário para inserir um número
         System.out.print("Digite um número: ");
 
-        // Ler o número digitado pelo usuário
         int numero = scanner.nextInt();
 
-        // Calcular o antecessor
         int antecessor = numero - 1;
 
-        // Exibir o resultado
         System.out.println("O antecessor de " + numero + " é: " + antecessor);
 
-        // Fechar o Scanner
         scanner.close();
     }
 }
