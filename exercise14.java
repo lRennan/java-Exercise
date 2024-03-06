56b2bc16361570950c377459f962f2f99ff09b10
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:01:06 -0300

    Update exercise14.java

--- exercise14.java
+++ exercise14.java
@@ -12,18 +12,14 @@ public class exercise14/*ReajusteSalarial*/ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura do salário mensal atual
         System.out.print("Digite o salário mensal atual: ");
         double salarioAtual = scanner.nextDouble();
 
-        // Leitura do percentual de reajuste
         System.out.print("Digite o percentual de reajuste: ");
         double percentualReajuste = scanner.nextDouble();
 
-        // Cálculo do novo salário
         double novoSalario = salarioAtual + (salarioAtual * percentualReajuste / 100);
 
-        // Exibição do novo salário
         System.out.println("O novo salário é: " + novoSalario);
 
         scanner.close();
