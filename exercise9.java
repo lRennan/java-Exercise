97ba996c852dc7642bd143de3a2cbb634a784c83
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:04:42 -0300

    Update exercise9.java

--- exercise9.java
+++ exercise9.java
@@ -5,20 +5,15 @@ import java.util.Scanner;
 
 public class exercise9 {
     public static void main(String[] args) {
-        // Criar um Scanner para ler as dimensões do quadrado
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar ao usuário para inserir o lado do quadrado
         System.out.print("Digite o lado do quadrado: ");
         double lado = scanner.nextDouble();
 
-        // Calcular a área do quadrado usando a fórmula: área = lado * lado
         double area = Math.pow(lado, 2);
 
-        // Exibir o resultado
         System.out.println("A área do quadrado com lado " + lado + " é: " + area);
 
-        // Fechar o Scanner
         scanner.close();
     }
 }
