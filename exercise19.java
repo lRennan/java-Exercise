100251c60e3ff91ec497aa75df1cf0954ff013d7
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:02:23 -0300

    Update exercise19.java

--- exercise19.java
+++ exercise19.java
@@ -16,24 +16,18 @@ public class exercise19 /* CalculoConsumoCombustivel */ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura do tempo gasto na viagem
         System.out.print("Digite o tempo gasto na viagem (em horas): ");
         double tempoViagem = scanner.nextDouble();
 
-        // Leitura da velocidade média
         System.out.print("Digite a velocidade média durante a viagem (em Km/h): ");
         double velocidadeMedia = scanner.nextDouble();
 
-        // Consumo do veículo (Km por litro)
         double consumoVeiculo = 12.0;
 
-        // Cálculo da distância percorrida
         double distanciaPercorrida = tempoViagem * velocidadeMedia;
 
-        // Cálculo da quantidade de litros de combustível
         double litrosConsumidos = distanciaPercorrida / consumoVeiculo;
 
-        // Exibição dos resultados
         System.out.println("Velocidade média: " + velocidadeMedia + " Km/h");
         System.out.println("Tempo gasto na viagem: " + tempoViagem + " horas");
         System.out.println("Distância percorrida: " + distanciaPercorrida + " Km");
