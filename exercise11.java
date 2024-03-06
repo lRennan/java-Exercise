d41b03213d05cdb7ec73507850df0adf53c5b8f4
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 16:59:46 -0300

    Update exercise11.java

--- exercise11.java
+++ exercise11.java
@@ -8,29 +8,22 @@ import java.util.Scanner;
 
 public class exercise11 {
     public static void main(String[] args) {
-        // Criar um Scanner para ler as dimensões do trapézio
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar ao usuário para inserir a base maior do trapézio
         System.out.print("Digite a base maior do trapézio: ");
         double baseMaior = scanner.nextDouble();
 
-        // Solicitar ao usuário para inserir a base menor do trapézio
         System.out.print("Digite a base menor do trapézio: ");
         double baseMenor = scanner.nextDouble();
 
-        // Solicitar ao usuário para inserir a altura do trapézio
         System.out.print("Digite a altura do trapézio: ");
         double altura = scanner.nextDouble();
 
-        // Calcular a área do trapézio usando a fórmula: área = (baseMaior + baseMenor) * altura / 2
         double area = (baseMaior + baseMenor) * altura / 2;
 
-        // Exibir o resultado
         System.out.println("A área do trapézio com base maior " + baseMaior +
                 ", base menor " + baseMenor + " e altura " + altura + " é: " + area);
 
-        // Fechar o Scanner
         scanner.close();
     }
 }
