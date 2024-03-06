6b34609dcbb133989d62f21b799481d13cdfe392
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:03:19 -0300

    Update exercise3.java

--- exercise3.java
+++ exercise3.java
@@ -8,24 +8,18 @@ import java.util.Scanner;
 
 public class exercise3 {
     public static void main(String[] args) {
-        // Criar um Scanner para ler os valores do teclado
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar ao usuário para inserir o primeiro número
         System.out.print("Digite o primeiro número: ");
         int primeiroNumero = scanner.nextInt();
 
-        // Solicitar ao usuário para inserir o segundo número
         System.out.print("Digite o segundo número: ");
         int segundoNumero = scanner.nextInt();
-
-        // Calcular a soma dos dois valores
+        
         int soma = primeiroNumero + segundoNumero;
 
-        // Exibir o resultado
         System.out.println("A soma de " + primeiroNumero + " e " + segundoNumero + " é: " + soma);
 
-        // Fechar o Scanner
         scanner.close();
     }
 }
