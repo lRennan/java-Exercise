17f96657c0dba4c81ee2a246c070a93c5a88a666
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:03:48 -0300

    Update exercise5.java

--- exercise5.java
+++ exercise5.java
@@ -11,24 +11,18 @@ import java.util.Scanner;
 
 public class exercise5 {
     public static void main(String[] args) {
-        // Criar um Scanner para ler os valores do teclado
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitar ao usuário para inserir o primeiro número
         System.out.print("Digite o primeiro número: ");
         double primeiroNumero = scanner.nextDouble();
 
-        // Solicitar ao usuário para inserir o segundo número
         System.out.print("Digite o segundo número: ");
         double segundoNumero = scanner.nextDouble();
 
-        // Calcular a multiplicação dos dois valores
         double multiplicacao = primeiroNumero * segundoNumero;
 
-        // Exibir o resultado
         System.out.println("A multiplicação de " + primeiroNumero + " e " + segundoNumero + " é: " + multiplicacao);
 
-        // Fechar o Scanner
         scanner.close();
     }
 }
