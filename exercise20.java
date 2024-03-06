23ee4357905efd2d6a666354e0434f6f55a10110
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-06 17:02:52 -0300

    Update exercise20.java

--- exercise20.java
+++ exercise20.java
@@ -11,22 +11,17 @@ public class exercise20 /*PrestacaoAtraso*/ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Leitura do valor da prestação
         System.out.print("Digite o valor da prestação: ");
         double valorPrestacao = scanner.nextDouble();
 
-        // Leitura da taxa de juros
         System.out.print("Digite a taxa de juros (em %): ");
         double taxaJuros = scanner.nextDouble();
 
-        // Leitura do tempo de atraso
         System.out.print("Digite o tempo de atraso (em meses): ");
         int tempoAtraso = scanner.nextInt();
 
-        // Cálculo do valor da prestação em atraso
         double valorPrestacaoAtraso = valorPrestacao + (valorPrestacao * taxaJuros / 100) * tempoAtraso;
 
-        // Exibição do valor da prestação em atraso
         System.out.println("O valor da prestação em atraso é: " + valorPrestacaoAtraso);
 
         scanner.close();
