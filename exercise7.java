5be2a8a1e1c9509eafc3d96372c3704bd2327889
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-15 14:53:03 -0300

    Update exercise7.java

--- exercise7.java
+++ exercise7.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise7 {
+public class exercise7 /*RetanguloArea*/ {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
