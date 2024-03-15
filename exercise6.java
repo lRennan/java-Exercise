c38150a8ea8b911f1346fa989ec4d68c6115a9fd
Rennan <94014673+lRennan@users.noreply.github.com>
2024-03-15 14:52:48 -0300

    Update exercise6.java

--- exercise6.java
+++ exercise6.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise6 {
+public class exercise6 /*Divisao*/{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
