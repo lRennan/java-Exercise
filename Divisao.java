55cd392e7856790cecfbbe8640ef7c30379efe7b
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:53:10 -0300

    Update and rename exercise06.java to Divisao.java

--- Divisao.java
+++ Divisao.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise6 /*Divisao*/{
+public class  Divisao{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
