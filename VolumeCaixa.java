b4178400f7a36b585d5f0e861b750bbf95500665
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:02:11 -0300

    Update and rename exercise23.java to VolumeCaixa.java

--- VolumeCaixa.java
+++ VolumeCaixa.java
@@ -7,7 +7,7 @@
 
 import java.util.Scanner;
 
-public class exercise23/*VolumeCaixa*/ {
+public class VolumeCaixa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
