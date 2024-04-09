bb0e99efb94eada3463512fe4ad58cc6cec5b308
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:07:39 -0300

    Update and rename exercise32.java to LerValorPositivoNegativo.java

--- LerValorPositivoNegativo.java
+++ LerValorPositivoNegativo.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise32 {
+public class LerValorPositivoNegativo {
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
         
