fa27c22c7f7750590000c2391a787fceceb73f4b
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:51:57 -0300

    Update exercise03.java

--- exercise03.java
+++ exercise03.java
@@ -6,7 +6,7 @@
 
 import java.util.Scanner;
 
-public class exercise3 /*Soma*/ {
+public class Soma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
