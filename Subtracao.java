48f31b92d5c3523d274e96cc165dd722f61851af
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:52:30 -0300

    Update and rename exercise04.java to Subtracao.java

--- Subtracao.java
+++ Subtracao.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class exercise4 /*Subtracao*/ {
+public class  Subtracao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
