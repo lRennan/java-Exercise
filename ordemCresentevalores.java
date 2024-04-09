5eb41c2d84918ea6334736520bf23ae158031414
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:11:34 -0300

    Update and rename exercise37.java to ordemCresentevalores.java

--- ordemCresentevalores.java
+++ ordemCresentevalores.java
@@ -3,7 +3,7 @@
  */
 
 import java.util.Scanner;
-public class exercise37 {
+public class ordemCresentevalores {
 	public static void main(String[]args) {
 		Scanner scanner = new Scanner(System.in);
 		
