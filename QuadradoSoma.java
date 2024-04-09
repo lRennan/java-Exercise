44269719fe542c12ef76eab990304304367f7ee3
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:04:26 -0300

    Update and rename exercise27.java to QuadradoSoma.java

--- QuadradoSoma.java
+++ QuadradoSoma.java
@@ -8,7 +8,7 @@
 
 import java.util.Scanner;
 
-public class exercise27/*QuadradoSoma*/ {
+public class QuadradoSoma {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
