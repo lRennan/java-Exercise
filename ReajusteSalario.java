575eea8a9f62f9fbb2d4d8add45af3d7c8ee12e6
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:04:49 -0300

    Update and rename exercise29.java to ReajusteSalario.java

--- ReajusteSalario.java
+++ ReajusteSalario.java
@@ -11,7 +11,7 @@
 
 import java.util.Scanner;
 
-public class exercise29/*ReajusteSalario*/ {
+public class ReajusteSalario {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
