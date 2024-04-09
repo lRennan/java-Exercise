009b38a652051a767ba1015f10fe30e00fe88e65
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:53:55 -0300

    Update and rename exercise08.java to TrianguloArea.java

--- TrianguloArea.java
+++ TrianguloArea.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise8 /*TrianguloArea*/{
+public class  TrianguloArea{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
