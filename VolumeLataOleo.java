865232b3212700f8014a2f9b252a09b9d495fa36
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:57:22 -0300

    Update and rename exercise18.java to VolumeLataOleo.java

--- VolumeLataOleo.java
+++ VolumeLataOleo.java
@@ -7,7 +7,7 @@
 
 import java.util.Scanner;
 
-public class exercise18 /*VolumeLataOleo*/ {
+public class VolumeLataOleo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
