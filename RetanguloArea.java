f6b81c4d14b15534c7c8831c15417ab8dfcda0c4
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 13:53:27 -0300

    Update and rename exercise07.java to RetanguloArea.java

--- RetanguloArea.java
+++ RetanguloArea.java
@@ -3,7 +3,7 @@
 */
 import java.util.Scanner;
 
-public class exercise7 /*RetanguloArea*/ {
+public class  RetanguloArea {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
