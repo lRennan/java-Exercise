0c214faa76c07f8b73cc8804970dfc25dc0945c1
Rennan <94014673+lRennan@users.noreply.github.com>
2024-04-09 14:01:21 -0300

    Update and rename exercise22.java to Distributiva.java

--- Distributiva.java
+++ Distributiva.java
@@ -7,7 +7,7 @@
 
 import java.util.Scanner;
 
-public class exercise22/*Distributiva*/ {
+public class Distributiva {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
